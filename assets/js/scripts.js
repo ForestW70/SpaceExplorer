b898d88e0019056502f8bb7c67ab29cc09f2c6ad
camjofindlay <camjofindlay@gmail.com>
2021-03-24 23:25:45 -0400

    widget addition w/ description of date, title, and description of picture. pushing HTML, CSS, and JS changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,3 +20,26 @@ fetch(astroPicTodayApi)
 // thankfully the api automatically returns todays picture, and we dont need to tie it to momentjs or anything.
 // hdurl is the link to the pic url, but there are other perameters we can display that you can look through in the console or through documentation.
 // in the future, we might want to look into makeing the fetch a function that we can call with the APi's url as an argument when we need it.
+
+function renderInfo() {
+  let today = moment()
+  let info = document.getElementById("description")
+  if (info.style.display === "none") {
+    fetch(astroPicTodayApi)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data.title);
+        console.log(data.explanation);
+        let head = data.title
+        let desc = data.explanation;
+        info.children[0].innerHTML = today.format("dddd, MMMM Do YYYY")
+        info.children[1].innerHTML = "Title: " + head
+        info.children[2].innerHTML = desc
+      });
+    info.style.display = "block";
+  } else {
+    info.style.display = "none";
+  }
+}
